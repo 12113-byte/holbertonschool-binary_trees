71bacb0659da0ee568a40f2515fdef791f1c7d97
Joleen <12113@holbertonstudents.com>
2025-12-04 03:52:17 +0000

    Task 1 update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node, old_left;
+	binary_tree_t *new_node, *old_left;
 
 	if (parent == NULL)
 	{
