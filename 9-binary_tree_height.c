1bf95d78f978930aa4bf2d029463386525e66ea0
Joleen <12113@holbertonstudents.com>
2025-12-08 06:17:04 +0000

    Task 9, added my own max func

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+int my_max(int value_one, int value_two)
+{
+	if (value_one > value_two)
+	{
+		return (value_one);
+	}
+	else
+	{
+		return (value_two);
+	}
+}
+
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
@@ -19,10 +31,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-		return (1 + left_height);
-	}
-	return (1 + right_height);
+	return (my_max(right_height, left_height) + 1);
 }
 
