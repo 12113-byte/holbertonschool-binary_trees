3d40e7f6aaa51d6d2b063ae2a1fe01f3c134cf25
Joleen <12113@holbertonstudents.com>
2025-12-08 05:54:51 +0000

    Task 9 fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,14 +16,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left)
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
 	{
-	left_height = 1 + binary_tree_height(tree->left);
+		return (1 + left_height);
 	}
-	if (tree->right)
-	{
-	right_height = 1 + binary_tree_height(tree->right);
-	}
-	return (left_height > right_height ? left_height : right_height);
+	return (1 + right_height);
 }
 
